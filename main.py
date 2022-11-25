971d73d0821e41034c9fae5f58f9628f11718f91
blacf0r7 <93528309+blacf0r7@users.noreply.github.com>
2022-11-25 17:53:50 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-# Подключение библиотек 
+# Подключение библиотек
 # from turtle import color
-import pygame 
+import pygame
 import time
 from random import randint
-
 
 # Инициализация библиотеки pygame
 pygame.init()
@@ -17,7 +16,7 @@ green = (120, 100, 50)
 
 # 
 dis_width = 800
-dis_height  = 400
+dis_height = 400
 
 print(randint(0, dis_width))
 
@@ -29,7 +28,7 @@ pygame.display.set_caption('Моя первая игра в змейку')
 
 game_over = False
 #  начальное положение
-x1 = (dis_width - min_Width)/4 
+x1 = (dis_width - min_Width) / 4
 y1 = dis_height / 2
 
 # Размер змейки
@@ -37,30 +36,31 @@ snake_block = 10
 
 x1_change = 0
 y1_change = 0
- 
+
 clock = pygame.time.Clock()
 snake_speed = 30
- 
-# Яблоко
-x_apple = dis_width/2
-y_apple = dis_height/2
 
+# Яблоко
+x_apple = 0
+y_apple = 0
+apple = False
 apple_size = 10
 
-def  randomApple():
-    x_apple = randint(0 , dis_width)
-    y_apple = randint(0 , dis_height)
 
+def randomApple():
+    x_apple = randint(0, dis_width)
+    y_apple = randint(0, dis_height)
 
 
 font_style = pygame.font.SysFont(None, 50)
-def message(msg,color):
+
+
+def message(msg, color):
     mesg = font_style.render(msg, True, color)
-    dis.blit(mesg, [dis_width/3.2, dis_height/2])
- 
+    dis.blit(mesg, [dis_width / 3.2, dis_height / 2])
+
+
 while not game_over:
-    x_apple = randint(0 , dis_width)
-    y_apple = randint(0 , dis_height)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game_over = True
@@ -77,9 +77,9 @@ while not game_over:
             elif event.key == pygame.K_DOWN:
                 y1_change = snake_block
                 x1_change = 0
-#  Условия Проигрыша
+    #  Условия Проигрыша
     if ((x1 > dis_width or x1 < 0) or (y1 > dis_height or y1 < 0)):
-        game_over =True
+        game_over = True
     x1 += x1_change
     y1 += y1_change
     dis.fill(white)
@@ -87,14 +87,20 @@ while not game_over:
     # Для добавления используем метод draw
     pygame.draw.rect(dis, black, [x1, y1, snake_block, snake_block])
     # Рисуем яблоко
-    pygame.draw.rect(dis, green, [x_apple, y_apple, apple_size, apple_size], 50)
+    if(apple  ==  False):
+        x_apple = randint(0, dis_width)
+        y_apple = randint(0, dis_height)
+        pygame.draw.rect(dis, green, [x_apple, y_apple, apple_size, apple_size], 50)
+        apple = True
+    
+
+
     pygame.display.update()
- 
     clock.tick(snake_speed)
- 
-message("Зачем нюк пикали",red)
+
+message("Зачем нюк пикали", red)
 pygame.display.update()
 time.sleep(2)
- 
+
 pygame.quit()
 quit()
